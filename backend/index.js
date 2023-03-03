91bbc88cdaef3da1dc9afe532457934b87814593
Alex Auch <alexsamuelauch@gmail.com>
2023-03-02 23:47:10 -0700

    set up sequelize config

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
-import express from 'express'
+const express = require("express")
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
 const app = express()
 
